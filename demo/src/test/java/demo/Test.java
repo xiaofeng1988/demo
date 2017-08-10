67b14eadd8b594cb21492e1aa3418e4fc8908472
xiaofeng <xiaofeng@192.168.0.10>
2017-08-10 17:19:43 +0800

    xinyi test begin

--- demo/src/test/java/demo/Test.java
+++ demo/src/test/java/demo/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("=================Test Begin=============");
 		System.out.println("=================Test Start=============");
-
+		System.out.println("=================Test End=============");
 	}
 
 }
