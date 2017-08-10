8efec401931f7e9c5984d0b605be5853543f7bed
xiaofeng_email@163.com <xiaofeng_email@163.com>
2017-08-10 17:23:17 +0800

    etst

--- demo/src/test/java/demo/Test.java
+++ demo/src/test/java/demo/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("=================Test Begin=============");
-		System.out.println("=================Test Start=============");
+		System.out.println("=================Test Begin===========");
+		System.out.println("=================Test Start===========");
 		System.out.println("=================Test End=============");
 	}
 
